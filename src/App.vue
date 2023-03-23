b7462e93af5bbb2592de367fbe797c1862549659
giovannitamburini <giovatambu@gmail.com>
2023-03-23 21:02:19 +0100

    completed ex without bonus

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+<script>
+// funzionalità
+// importo il componente indicando il percorso e il nome
+import Title from "./components/AppTitle.vue";
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+  // aggiungiamo il componente, fuori dai data in un oggetto apposito chiamato 'components'
+  components: {
+    Title,
+  },
+};
 </script>
 
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- layout -->
+  <Title></Title>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+/* stile */
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style>
+h1 {
+    color: #0000dd;
+}
+</style>
